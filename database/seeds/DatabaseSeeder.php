ff258bd12f7b4db5705233308435779d7a7a6b6f
Mulia Arifandi Nasution <mul14.net@gmail.com>
2016-03-18 07:58:15 +0700

    Add Seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,5 +12,9 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+
+        factory('App\Doctor')->create(['nip' => 'doc', 'password' => bcrypt('123456')]);
+        factory('App\Patient')->create(['nip' => 'pat', 'password' => bcrypt('123456')]);
+        factory('App\Staff')->create(['nip' => 'stf', 'password' => bcrypt('123456')]);
     }
 }

ba501946e39e1d51e69e1fe3d879ca6957d20ae1
Mulia Arifandi Nasution <mul14.net@gmail.com>
2016-03-18 08:04:54 +0700

    Add Authenticatable to Doctor, Patient, and Staff model.

--- app/Staff.php
+++ app/Staff.php
@@ -2,9 +2,25 @@
 
 namespace App;
 
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class Staff extends Model
+class Staff extends Authenticatable
 {
-    //
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'nip', 'name', 'email', 'password',
+    ];
+
+    /**
+     * The attributes excluded from the model's JSON form.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
 }

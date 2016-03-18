3f9cd0bed55c1711e31c37afb1d8532555114121
Mulia Arifandi Nasution <mul14.net@gmail.com>
2016-03-18 08:13:46 +0700

    Add routes

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,2 +1,55 @@
 <?php
 
+Route::get('/', function(){
+
+return <<<HTML
+<ul>
+    <li> <a href="/login/doctor">Login as doctor</a> </li>
+    <li> <a href="/login/patient">Login as patient</a> </li>
+    <li> <a href="/login/staff">Login as staff</a> </li>
+</ul>
+HTML;
+
+});
+
+Route::get('/login/doctor', function() {
+
+    $credentials = [
+        'nip' =>  'doc',
+        'password' =>  '123456',
+    ];
+
+    if (auth('doctor')->attempt($credentials)) {
+        return 'Welcome, doctor.';
+    }
+
+    return 'Gagal login.';
+});
+
+Route::get('/login/patient', function() {
+
+    $credentials = [
+        'nip' =>  'pat',
+        'password' =>  '123456',
+    ];
+
+    if (auth('patient')->attempt($credentials)) {
+        return 'Welcome, patient.';
+    }
+
+    return 'Gagal login.';
+});
+
+Route::get('/login/staff', function() {
+
+    $credentials = [
+        'nip' =>  'stf',
+        'password' =>  '123456',
+    ];
+
+    if (auth('staff')->attempt($credentials)) {
+        return 'Welcome, staff.';
+    }
+
+    return 'Gagal login.';
+});
